014b5ff399e59e8977513709d68bd6463c232048
ayush <ayu023ban@gmail.com>
2019-12-14 13:44:17 +0530

    added ordering page withoout css

--- orderprocess.php
+++ orderprocess.php
@@ -9,7 +9,7 @@ if($conn->connect_error){
 } 
 session_start();
 
-if(isset($_SESSION['email'])){
+
     $a1="<a class=\"hover-color right-nav gallery\">welcome ".$_SESSION['email']."</a>";
     $a2="<form action=\"process.php\" method=\"get\" >
           <button type=\"submit\" class=\"logout\" name=\"logout\">
@@ -18,12 +18,97 @@ if(isset($_SESSION['email'])){
           </form> ";
                   
           
+  
+
+
+
+
+if(!empty($_GET["action"])) {
+switch($_GET["action"]) {
+	case "add":
+		if(!empty($_POST["quantity"])) {
+      $result1 = $conn->query("SELECT * FROM products WHERE code='" . $_GET["code"] . "'");
+    while($row = $result1->fetch_assoc()) {
+        $productByCode[]=$row;
+       
+    }
+			
+    $itemArray = array($productByCode[0]["code"]=>array('name'=>$productByCode[0]["name"],'id'=>$productByCode[0]["id"], 'code'=>$productByCode[0]["code"], 'quantity'=>$_POST["quantity"], 'price'=>$productByCode[0]["price"], 'image'=>$productByCode[0]["image"]));      
+			
+			
+			if(!empty($_SESSION["cart_item"])) {
+				if(in_array($productByCode[0]["code"],array_keys($_SESSION["cart_item"]),false)) {
+					foreach($_SESSION["cart_item"] as $k => $v) {
+							if($productByCode[0]["code"] == $k) {
+								if(empty($_SESSION["cart_item"][$k]["quantity"])) {
+									$_SESSION["cart_item"][$k]["quantity"] = 0;
+								}
+								$_SESSION["cart_item"][$k]["quantity"] += $_POST["quantity"];
+							}
+					}
+				} else {
+					$_SESSION["cart_item"][$productByCode[0]["code"]] =$itemArray[$productByCode[0]["code"]];
+				}
+			} else {
+				$_SESSION["cart_item"][$productByCode[0]["code"]] = $itemArray[$productByCode[0]["code"]];
+			}
+		}
+	break;
+	case "remove":
+		if(!empty($_SESSION["cart_item"])) {
+			foreach($_SESSION["cart_item"] as $k => $v) {
+					if($_GET["code"] == $k)
+						unset($_SESSION["cart_item"][$k]);				
+					if(empty($_SESSION["cart_item"]))
+						unset($_SESSION["cart_item"]);
+			}
+		}
+	break;
+	case "empty":
+		unset($_SESSION["cart_item"]);
+	break;	
+}
+}
+
+
+
+
+
+if(isset($_POST["order"])){
+  $result = $conn->query("SELECT userindex FROM users where email='".$_SESSION['email']."';");
+  while($row = $result->fetch_assoc()) {
+    $user_array[]=$row;
+}
+  $userid=$user_array[0]["userindex"];
+  $noOfMembers=$_POST['members'];
+  $time=$_POST['time'];
+
+
+  for($i=1;$i<17;$i++) {
+    foreach ($_SESSION["cart_item"] as $value) {
+      if($i==$value['id']){
+        $ordersummary["$i"]=$value['quantity'];
+      break;
+        }
+        else{
+          $ordersummary["$i"]=0;
+        }
+     
   }
-  else{
-    $a1="";
-    $a2="<a href=\"registration.php\"   class=\"hover-color right-nav gallery\">Sign Up</a>";
+  
+  }
    
-  }
+
+  $sql="INSERT INTO `orderSummary` ( `userid`, `noOfMembers`, `timeOfComming`, `Hors d'oeuvres`, `Amuse-bouche`, `Bouillabaisse`, `Feijoada`, `Leche Asada`, `Lemon Meringue Pie`, `Brigadeiros`, `Bunuelos`, `HOT TODDY`, `The Hornitos Hot Shotty`, `LAST-MINUTE MULLED WINE`, `HEATED AFFAIR`, `Guinness Of Ireland`, `Pina Colada`, `GIN of ENGLAND`, `Campari`) VALUES 
+   ($userid ,$noOfMembers,'$time',$ordersummary[1],$ordersummary[2],$ordersummary[3],$ordersummary[4],$ordersummary[5],$ordersummary[6],$ordersummary[7],$ordersummary[8],$ordersummary[9],$ordersummary[10],$ordersummary[11],$ordersummary[12],$ordersummary[13],$ordersummary[14],$ordersummary[15],$ordersummary[16])";
+  if ($conn->query($sql) === TRUE) {
+    echo "Your data is saved succesfully.";
+} else {
+    echo "Error: " . $sql . "<br>" . $conn->error;
+}
+}
+
+
 
 
 
--- table.php
+++ table.php
@@ -3,11 +3,13 @@
 	header('location: login.php');
 }
 ?>
+
 <html lang="en">
 
 <head>
     <meta charset="UTF-8">
-    <link rel="stylesheet" href="drinkscss.css">
+    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="visitcss.css">
     <link href="font.css" rel="stylesheet">
     <script src="fontawesome.js"></script>
     <link rel="stylesheet" href="css1.css" media="all">
@@ -40,7 +42,107 @@
             </div>
         </div>
         <div class="mainbody">
+        <div class="container">
             
+          
+        
+
+        <div id="shopping-cart">
+<div class="txt-heading">Ordered Food</div>
+
+<a id="btnEmpty" href="table.php?action=empty">Empty Cart</a>
+<?php
+if(isset($_SESSION["cart_item"])){
+    $total_quantity = 0;
+    $total_price = 0;
+?>	
+<table class="tbl-cart" cellpadding="10" cellspacing="1">
+<tbody>
+<tr>
+<th style="text-align:left;">Name</th>
+<th style="text-align:left;">    </th>
+<th style="text-align:right;" width="5%">Quantity</th>
+<th style="text-align:right;" width="10%">Unit Price</th>
+<th style="text-align:right;" width="10%">Price</th>
+<th style="text-align:center;" width="5%">Remove</th>
+</tr>	
+<?php		
+    foreach ($_SESSION["cart_item"] as$key=> $item){
+        $item_price = $item["quantity"]*$item["price"];
+		?>
+				<tr>
+				<td><img src="<?php echo $item["image"]; ?>" class="cart-item-image" /><?php echo $item["name"]; ?></td>
+				<td><?php echo $item["code"]; ?></td>
+				<td style="text-align:right;"><?php echo $item["quantity"]; ?></td>
+				<td  style="text-align:right;"><?php echo " ".$item["price"]; ?></td>
+				<td  style="text-align:right;"><?php echo "$ ". number_format($item_price,2); ?></td>
+				<td style="text-align:center;"><a href="table.php?action=remove&code=<?php echo $item["code"]; ?>" class="btnRemoveAction"><img src="icon-delete.png" alt="Remove Item" /></a></td>
+				</tr>
+				<?php
+				$total_quantity += $item["quantity"];
+				$total_price += ($item["price"]*$item["quantity"]);
+		}
+		?>
+
+<tr>
+<td colspan="2" align="right">Total:</td>
+<td align="right"><?php echo $total_quantity; ?></td>
+<td align="right" colspan="2"><strong><?php echo "$ ".number_format($total_price, 2); ?></strong></td>
+<td></td>
+</tr>
+</tbody>
+</table>
+<form method='post' action="table.php">
+<label for="members"><b>How many Will come?</b></label><br>
+    <input type="number" name="members" required><br>
+    <label for="time"><b>When will you come?</b></label><br>
+    <input type="datetime-local" name="time" required><br>
+    <input  class="table-button" type="submit" name="order" value="ODER YOUR FOOD">
+
+
+  <?php
+} else {
+?>
+
+<div class="no-records">Your Cart is Empty</div>
+<?php 
+}
+?>
+</div>
+            
+
+
+
+
+
+
+
+            <div class="txt-heading">Products</div>
+	<?php
+    $sql="SELECT * FROM products ORDER BY id ASC";
+    $result = $conn->query($sql);
+    while($row = $result->fetch_assoc()) {
+        $product_array[]=$row;
+    }
+	if (!empty($product_array)) { 
+		foreach($product_array as $key=>$value){
+	?>
+		<div class="product-item">
+            <?php echo $product_array[$key]["code"]; ?>
+			<form method="post" action="table.php?action=add&code=<?php echo $product_array[$key]["code"]; ?>">
+			<div class="product-image"><img src="<?php echo $product_array[$key]["image"]; ?>"></div>
+			<div class="product-tile-footer">
+			<div class="product-title"><?php echo $product_array[$key]["name"]; ?></div>
+			<div class="product-price"><?php echo "$".$product_array[$key]["price"]; ?></div>
+			<div class="cart-action"><input type="text" class="product-quantity" name="quantity" value="1" size="2" /><input type="submit" value="Add to Cart" class="btnAddAction" /></div>
+			</div>
+			</form>
+		</div>
+	<?php
+		}
+	}
+	?>
+</div>
             <hr class="thin"><a href="restest.php" class="title-footer"> —A Y U S H 'S— </a>
             <div class="social-div">
                     <a href="https://twitter.com/ayushbansal023" ><i id="twitter"  class="fab fa-twitter social-nav" aria-hidden="true"></i></a>
@@ -49,7 +151,7 @@
             </div>
         </div>
     </div>
-    <script type="text/javascript" src="main.js"></script>
+    
 
 </body>
 
